2789f6717898cd581d8b480c5168d200d12295bb
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-31 10:53:49 +0100

    chore: create favourite slice

---
+++ store/redux/favouriteSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  ids: [],
+};
+
+const favouriteSlice = createSlice({
+  name: "favourite",
+  initialState,
+
+  reducers: {
+    addToFavouriteMeals: (state, action) => {
+      state.ids.push(action.payload);
+    },
+
+    deleteFromFavouriteMeals: (state, action) => {
+      state.ids = state.ids.filter((id) => id !== action.payload.id);
+    },
+  },
+});
+
+export default favouriteSlice.reducer;
+
+export const { addToFavouriteMeals, deleteFromFavouriteMeals } =
+  favouriteSlice.actions;
