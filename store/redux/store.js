b09fe1d5e361e681527382ad6c274fdc6c455a18
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-10-31 10:55:12 +0100

    chore: add favourite resucer to the store

--- store/redux/store.js
+++ store/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import favouriteSlice from "./favouriteSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    favourite: favouriteSlice,
+  },
 });
